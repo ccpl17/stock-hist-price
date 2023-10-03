6f12db22f93a3bfe21db7528f34a63034630e856
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2023-10-03 18:18:48 +0800

    發布：23.10.0

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main(page: Page):
     page.theme_mode = ThemeMode.SYSTEM
     page.scroll = ScrollMode.AUTO
 
-    current_version = "23.9.1"
+    current_version = "23.10.0"
 
     endpoint = "https://gist.githubusercontent.com/ccpl17/be3362652dd53ad5f63962993b145a0e/raw/"
 
